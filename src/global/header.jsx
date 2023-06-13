f65b46c684f0442ff70266eef07c69cb6cee03b8
Sandra Jasmin <sandra-jasmin@hotmail.com>
2023-06-13 23:22:36 +0200

    add about section

--- src/global/header.jsx
+++ src/global/header.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-import { NavLink } from 'react-router-dom'
 
 export default function Header() {
     const [isMenuOpen, setIsMenuOpen] = useState(false)
--- src/pages/homepage/index.jsx
+++ src/pages/homepage/index.jsx
@@ -1,15 +1,12 @@
 import React from 'react'
-import HeroDesktop from '../../assets/desktop.png'
+import HeroSection from './components/heroSection'
+import AboutSection from './components/AboutSection'
 
 export default function TextWithImage() {
     return (
         <>
-            <div className="flex justify-center px-3 py-10 lg:py-28">
-                <img
-                    src={HeroDesktop}
-                    alt="Sandra Jasmin in text with profile in background"
-                />
-            </div>
+            <HeroSection />
+            <AboutSection />
         </>
     )
 }
