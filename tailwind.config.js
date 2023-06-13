85939809f3a6d2e79998b1a7d8599278c7aaa865
Sandra Jasmin <sandra-jasmin@hotmail.com>
2023-06-13 13:41:33 +0200

    initialize project

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      'serif': ['Libre Caslon Text'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
